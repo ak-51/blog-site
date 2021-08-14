cb68d8c4656efa0867b462b71a49b9574c9bd65b
ak-51 <aditya.kumar.752cz@gmail.com>
2021-08-14 12:24:55 +0800

    finished blog creation frontend

--- client/src/components/CreateBlog.jsx
+++ client/src/components/CreateBlog.jsx
@@ -5,13 +5,19 @@ const CreateBlog = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
 
+    const blogSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return(
         <Fragment>
             <div className="createPage">
+                <p className="createTitle">Create Blog</p>
                 <div className="createContainer">
-                    <form className="createForm">
+                    <form className="createForm" onSubmit={blogSubmit}>
                         <input className="createFormTitle" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-                        <input className="createFormContent" type="text" placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)} />
+                        <textarea name="content" id="createFormContent" cols="100" rows="20" placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                        <button>Submit</button>
                     </form>
                 </div>
             </div>
